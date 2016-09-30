19fdefaf624f4ccc559496f3d67e080db364203b
crabcrabcam <cam12win@gmail.com>
2016-09-30 14:46:01 +0100

    Movement
    Added the graphics and spent forever working on the movement. It was a
    pain in the arse…

---
+++ Assets/Scripts/ControlCanvas.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlCanvas : MonoBehaviour {
+
+	public float movex;
+	public float movey;
+
+	public void upPressed() {
+		movey = 1;
+	}
+	public void upLetGoOf() {
+		movey = 0;
+	}
+
+	public void downPressed() {
+		movey = -1;
+	}
+
+	public void downLetGoOf() {
+		movey = 0;
+	}
+
+	public void leftPressed() {
+		movex = -1;
+	}
+
+	public void leftLetGoOf() {
+		movex = 0;
+	}
+
+	public void rightPressed() {
+		movex = 1;
+	}
+
+	public void rightLetGoOf() {
+		movex = 0;
+	}
+
+	public void attackPressed() {
+		//This will attack
+		print("This command will attack using the current weapon!");
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float movex;
+	public float movey;
+	public float speed;
+
+	public ControlCanvas controlCanvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		Movement ();
+	}
+
+
+	void Movement() {
+
+		movex = controlCanvas.movex * speed;
+		movey = controlCanvas.movey * speed;
+
+
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (movex, movey);
+
+//		transform.Translate (movex, movey, 0);
+
+
+	}
+}
